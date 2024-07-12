1e2e3802c69fdfa49be60d2be474a5d0d70b32fe
uezo <uezo@uezo.net>
2024-07-12 21:05:56 +0900

    Update for v0.2.0🙌

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="linedify",
-    version="0.1.1",
+    version="0.2.0",
     url="https://github.com/uezo/linedify",
     author="uezo",
     author_email="uezo@uezo.net",
