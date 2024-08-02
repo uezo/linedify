656624c93695290040ed367e094a3fa693e6362a
uezo <uezo@uezo.net>
2024-08-01 19:55:13 -0700

    Revert "v0.2.5🌈🏝️🌺"
    This reverts commit 44e5b9bcbeaaccf7a0c3bc47a1fb6babef056bcc.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="linedify",
-    version="0.2.5",
+    version="0.2.3",
     url="https://github.com/uezo/linedify",
     author="uezo",
     author_email="uezo@uezo.net",
